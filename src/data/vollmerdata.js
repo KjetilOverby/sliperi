6675222d1d3a61f00b5a9822a1314a2ee59a7e88
Kjetil  Overby <kjeoever@msn.com>
2022-09-07 10:25:54 +0200

    service

--- src/data/vollmerdata.js
+++ src/data/vollmerdata.js
@@ -1,26 +1,51 @@
 const vollmer = [
-    {
-        date: '20.08.2020',
-        title: 'Installering av ny slipemaskin',
-        description: 'En fra Vollmer kommer å installerer den nye slipemaskinen og kurser oss i å bruke den. Han kommer hit i 2 dager.'
-        
-    },
-    {
-        date: '16.11.2020',
-        title: 'Filteranlegg til Vollmer 1300',
-        description: 'Filteranlegger til den nye slipemaskinen har ankommet, men kan ikke koble den på før vi har fått mer olje til maskinen.',
-        img: 'https://cdn.innocode.digital/hesselberg/uploads/sites/3/2020/01/maskin-focalcropped-e1427439059925-2157x1100-2157x1100.jpg'
-    },
-    {
-        date: '02.02.2021',
-        title: 'Koblet til Filter på slipemaskin',
-        description: 'Mer kjøleolje til slipemakinen har kommet i dag, Dag olav kobler til filteranlegget.'
-    },
-    {
-        date: '15.09.2021',
-        title: 'Service på slipemaskin (Vollmer 1300)',
-        description: 'Det kom en servicemann fra Tyskland som tok en service på slipemaskinen. Han har vært hos Stridsbergs men han måtte vente på noen deler så han kombinerte det med å ta service på vår maskin mens han venter på deler.'
-    }
-]
+  {
+    date: "04.08.2020",
+    title: "Ny slipemaskin",
+    description:
+      "Ny slipemaskin har ankommet Moelven Våler i dag. Det er en Vollmer 1300. Utfordringen nå er å få denne inn på sliperiet.",
+    img: "https://lh3.googleusercontent.com/pw/AL9nZEXhdEg8B1j-95ccQOPiMxLFiCdOYXIQGj2yZxAK5orBTtD29_1emSy29oiF6-UVJS7JW0qG3Z01KwrSJUHRQ4xdVrb6wMNg_VbsgLNNI4ajL-TyiO9Md0tn99ovN9Jj1qXkrt5IBTYETyq21WuoYGGZ=w1440-h1080-no?authuser=0",
+  },
+  {
+    date: "05.08.2020",
+    title: "Plassering av slipemaskin",
+    description:
+      "Det var meningen at vi skulle ha slipemaskinen i rommet med betonggulv men der ble takhøyden for lav. Vi må da sette maskinen der den gamle står i det andre rommet. Varemottaket er også for lavt så vi må skjære av litt i høyden. Tor Engebretsen tar seg av det med hjelp fra Lars Andre skjæret.",
+    img: "https://lh3.googleusercontent.com/pw/AL9nZEXL5U9UIbBv6SyU1QhfSLzWPCh_nzfBvnUWPBtlMPFh_D2mnzM1-PsXfqkJQc1DQg_tzZi6cNnKzW_yMZ2v_nG1RDMKs3Nw8BsCb6zhKv71JmmSOcN6KQtWSy6BPjtGzn9Ra41mQurmtEVGSGFuovZ-=w1440-h1080-no?authuser=0",
+  },
+  {
+    date: "07.08.2020",
+    title: "Slipemaskin på plass",
+    description:
+      "Slipemaskinen ble satt på plass, Kjell Holter kjører trucken og løfter den inn. Litt utfordringer å få den inn, men det gikk fint til slutt.",
+    img: "https://lh3.googleusercontent.com/pw/AL9nZEXcQmBNuzM2hWjwCssf7fzj_0lbQdkFVZAVvBxUX38xVS2PSB-ej2aBmNS5LlPbK5A_JMt2FbnR3N8dacFy368zDnwaIkxX6kq5RSg9jSr3-El6ZtqasnaWRpWgx5rlKDN33F0FLsTlIGkhdbkOHEW8=w1440-h1080-no?authuser=0",
+  },
+  {
+    date: "20.08.2020",
+    title: "Installering av ny slipemaskin",
+    description:
+      "En fra Vollmer kommer å installerer den nye slipemaskinen og kurser oss i å bruke den. Han kommer hit i 2 dager.",
+  },
+  {
+    date: "16.11.2020",
+    title: "Filteranlegg til Vollmer 1300",
+    description:
+      "Filteranlegger til den nye slipemaskinen har ankommet, men kan ikke koble den på før vi har fått mer olje til maskinen.",
+    img: "https://lh3.googleusercontent.com/pw/AL9nZEVp-mSzs5a6opM4r3co2VBISUF56q4JWlR5Nu_5k8eIKpZBWm_dRC0u-x8ZL8upnazlYP5iFwV9ntVVkdpstO44uHV6dOZ6mz5jKd7JhH4YQEeMFvWH14IVu3e0v3t-c9TCzPfGrswEvxXWp9taGURJ=w1919-h1080-no?authuser=0",
+  },
+  {
+    date: "02.02.2021",
+    title: "Koblet til Filter på slipemaskin",
+    description:
+      "Mer kjøleolje til slipemakinen har kommet i dag, Dag olav kobler til filteranlegget.",
+  },
+  {
+    date: "15.09.2021",
+    title: "Service på slipemaskin (Vollmer 1300)",
+    description:
+      "Det kom en servicemann fra Tyskland som tok en service på slipemaskinen. Han har vært hos Stridsbergs men han måtte vente på noen deler så han kombinerte det med å ta service på vår maskin mens han venter på deler.",
+    tag: "service",
+  },
+];
 
-export default vollmer
+export default vollmer;
