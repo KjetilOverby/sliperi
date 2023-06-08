3b587029d54abf2aae5f13236cee9c6f83267236
KjetilOverby <kjeoever@msn.com>
2023-06-08 12:56:18 +0200

    this year

--- pages/statistikk.js
+++ pages/statistikk.js
@@ -1,14 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageLayoutSidebar from "../src/components/common/PagelayoutSidebar";
 import SideBar from "../src/components/common/Sidebar";
 import StatistikkMainPage from "../src/components/statistikk/StatistikkMainPage";
+const axios = require("axios");
+const api = axios.create({
+  baseURL: process.env.api,
+});
 
 const statistikk = () => {
+  const [wasteThisYear, setWasteThisYear] = useState();
+  const [serviceThisYear, setServiceThisYear] = useState();
+  const [newThisYear, setNewThisYear] = useState();
+
+  const [toolsWaste, setToolsWaste] = useState();
+
+  const currentYear = new Date().getFullYear();
+
+  console.log(wasteThisYear);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get(
+          `/api/datepicker/datepickerwaste?yearRequest=${currentYear}&&month=01&&day=01&&yearRequest2=${currentYear}&&month2=12&&day2=12`
+        );
+        setWasteThisYear(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentYear]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get(
+          `/api/datepicker/datepickerservice?yearRequest=${currentYear}&&month=01&&day=01&&yearRequest2=${currentYear}&&month2=12&&day2=12`
+        );
+        setServiceThisYear(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentYear]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get(
+          `/api/datepicker/datepickernew?yearRequest=${currentYear}&&month=01&&day=01&&yearRequest2=${currentYear}&&month2=12&&day2=12`
+        );
+        setNewThisYear(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentYear]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get(
+          `/api/tool/datepickertools/datepickertoolswaste?yearRequest=${currentYear}&&month=01&&day=01&&yearRequest2=${currentYear}&&month2=12&&day2=12`
+        );
+        setToolsWaste(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentYear]);
+
   return (
     <>
       <PageLayoutSidebar>
         <SideBar />
-        <StatistikkMainPage />
+        <StatistikkMainPage
+          currentYear={currentYear}
+          wasteThisYear={wasteThisYear}
+          serviceThisYear={serviceThisYear}
+          newThisYear={newThisYear}
+          toolsWaste={toolsWaste}
+        />
       </PageLayoutSidebar>
       <style jsx>
         {`
--- src/components/statistikk/StatistikkMainPage.jsx
+++ src/components/statistikk/StatistikkMainPage.jsx
@@ -1,77 +1,141 @@
 import React from "react";
 import stats from "../../data/stats";
 
-const StatistikkMainPage = () => {
+const StatistikkMainPage = ({
+  wasteThisYear,
+  serviceThisYear,
+  newThisYear,
+  currentYear,
+  toolsWaste,
+}) => {
+  console.log(toolsWaste);
   return (
     <>
       <div className="main-container">
         <h1 className="stat-header">Statistikk årlig</h1>
-        {stats.map((item) => {
-          return (
-            <div key={item.year} className="year-container">
-              <div>
-                <h4>{item.year}</h4>
-                <p className="sub-header">Vrak ({item.allVrk}):</p>
-                {item.vrkList.map((item) => {
-                  return (
-                    <div key={item.typeCount} className="tool-text-container">
-                      <p className="tool-text count">{item.typeCount}</p>
-                      <p className="tool-text type">{item._id.type}</p>
-                    </div>
-                  );
-                })}
+        <div className="year-container">
+          <div>
+            <h4>Hittil i år ({currentYear})</h4>
+            <p className="sub-header">Vrak:</p>
+            {wasteThisYear &&
+              wasteThisYear.map((item) => {
+                return (
+                  <div key={item.typeCount} className="tool-text-container">
+                    <p className="tool-text count">{item.typeCount}</p>
+                    <p className="tool-text type">{item._id.type}</p>
+                  </div>
+                );
+              })}
+          </div>
+          <div>
+            <h4 style={{ visibility: "hidden" }}>Hittil i år</h4>
+            <p className="sub-header">Service:</p>
+            {serviceThisYear &&
+              serviceThisYear.map((item) => {
+                return (
+                  <div key={item.typeCount} className="tool-text-container">
+                    <p className="tool-text count">{item.typeCount}</p>
+                    <p className="tool-text type">{item._id.type}</p>
+                  </div>
+                );
+              })}
+          </div>
+          <div>
+            <h4 style={{ visibility: "hidden" }}>Hittil i år</h4>
+            <p className="sub-header">Nye:</p>
+            {newThisYear &&
+              newThisYear.map((item) => {
+                return (
+                  <div key={item.typeCount} className="tool-text-container">
+                    <p className="tool-text count">{item.typeCount}</p>
+                    <p className="tool-text type">{item._id.type}</p>
+                  </div>
+                );
+              })}
+          </div>
+          <div>
+            <h4 style={{ visibility: "hidden" }}>Hittil i år</h4>
+            <p className="sub-header">Vrak segmenter og kniver:</p>
+            {toolsWaste &&
+              toolsWaste.map((item) => {
+                return (
+                  <div key={item.typeCount} className="tool-text-container">
+                    {/*  <p className="tool-text count">{item.type}</p>
+                    <p className="tool-text type">{item.type}</p> */}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        {stats
+          .slice(0)
+          .reverse()
+          .map((item) => {
+            return (
+              <div key={item.year} className="year-container">
+                <div>
+                  <h4>{item.year}</h4>
+                  <p className="sub-header">Vrak ({item.allVrk}):</p>
+                  {item.vrkList.map((item) => {
+                    return (
+                      <div key={item.typeCount} className="tool-text-container">
+                        <p className="tool-text count">{item.typeCount}</p>
+                        <p className="tool-text type">{item._id.type}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
+                  <p className="sub-header">Service ({item.allSrv}):</p>
+                  {item.srvList.map((item) => {
+                    return (
+                      <div key={item.typeCount} className="tool-text-container">
+                        <p className="tool-text count">{item.typeCount}</p>
+                        <p className="tool-text type">{item._id.type}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
+                  <p className="sub-header">Nye blad ({item.allNew}):</p>
+                  {item.newList.map((item) => {
+                    return (
+                      <div key={item.typeCount} className="tool-text-container">
+                        <p className="tool-text count">{item.typeCount}</p>
+                        <p className="tool-text type">{item._id.type}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
+                  <p className="sub-header">Vrak segmenter og kniver:</p>
+                  {item.toolsVrak.map((item) => {
+                    return (
+                      <div key={item.typeCount} className="tool-text-container">
+                        <p className="tool-text count">{item.ant}</p>
+                        <p className="tool-text type">{item.type}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
+                  <p className="sub-header">Nye segmenter og kniver:</p>
+                  {item.toolsNew.map((item) => {
+                    return (
+                      <div key={item.typeCount} className="tool-text-container">
+                        <p className="tool-text count">{item.ant}</p>
+                        <p className="tool-text type">{item.type}</p>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-              <div>
-                <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
-                <p className="sub-header">Service ({item.allSrv}):</p>
-                {item.srvList.map((item) => {
-                  return (
-                    <div key={item.typeCount} className="tool-text-container">
-                      <p className="tool-text count">{item.typeCount}</p>
-                      <p className="tool-text type">{item._id.type}</p>
-                    </div>
-                  );
-                })}
-              </div>
-              <div>
-                <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
-                <p className="sub-header">Nye blad ({item.allNew}):</p>
-                {item.newList.map((item) => {
-                  return (
-                    <div key={item.typeCount} className="tool-text-container">
-                      <p className="tool-text count">{item.typeCount}</p>
-                      <p className="tool-text type">{item._id.type}</p>
-                    </div>
-                  );
-                })}
-              </div>
-              <div>
-                <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
-                <p className="sub-header">Vrak segmenter og kniver:</p>
-                {item.toolsVrak.map((item) => {
-                  return (
-                    <div key={item.typeCount} className="tool-text-container">
-                      <p className="tool-text count">{item.ant}</p>
-                      <p className="tool-text type">{item.type}</p>
-                    </div>
-                  );
-                })}
-              </div>
-              <div>
-                <h4 style={{ visibility: "hidden" }}>{item.year}</h4>
-                <p className="sub-header">Nye segmenter og kniver:</p>
-                {item.toolsNew.map((item) => {
-                  return (
-                    <div key={item.typeCount} className="tool-text-container">
-                      <p className="tool-text count">{item.ant}</p>
-                      <p className="tool-text type">{item.type}</p>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
       <style jsx>
         {`
@@ -79,16 +143,20 @@ const StatistikkMainPage = () => {
             grid-area: content;
             background: var(--secondary);
             padding: 1rem;
+            overflow: scroll;
+            height: 100vh;
           }
           .year-container {
             margin-bottom: 1rem;
             display: grid;
             grid-template-columns: repeat(auto-fit, minmax(250px, 10rem));
             padding: 2rem;
-            background: var(--secondary2);
+            background: var(--primary);
+            border-radius: 10px;
           }
           .sub-header {
             color: var(--middle);
+            margin-bottom: 1rem;
           }
           .stat-header {
             color: var(--middle);
